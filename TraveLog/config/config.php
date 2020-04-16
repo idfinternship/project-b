e48b523cae22217d20861bccbd05c2787c5f69ce
ViliusGr <vilius.grimalauskas@gmail.com>
2020-04-16 12:19:39 +0300

    verfication/crud/country selection

--- TraveLog/config/config.php
+++ TraveLog/config/config.php
@@ -1,5 +1,5 @@
 <?php 
-	define('ROOT_URL', 'http://localhost/project-b/TraveLog/');
+	define('ROOT_URL', 'http://localhost/TraveLog/');
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', 'password');
