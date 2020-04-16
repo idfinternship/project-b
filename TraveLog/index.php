5300de76dd3c89613b388e3d704d45e70c921d7c
Necrorez <56365008+Necrorez@users.noreply.github.com>
2020-04-16 10:32:14 +0300

    Add files via upload

--- TraveLog/index.php
+++ TraveLog/index.php
@@ -1,137 +1,139 @@
 <?php require("login_session.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>TraveLog test</title>
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <script src="js/three.min.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/gio.js"></script>     
-    <?php require('style.php'); ?>
-    <?php require('navbar.php'); ?>
+    <head>
+        <meta charset="UTF-8">
+        <title>TraveLog test</title>
 
-<!-- Info lenteles salim -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
+        <script src="js/three.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/gio.js"></script>
+        <?php require('style.php'); ?>
+            <?php require('navbar.php'); ?>
 
-#search, #textbox{
+                <!-- Info lenteles salim -->
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <style>
+                    #search,
+                    #textbox {
+                        position: absolute;
+                        height: 40px;
+                        line-height: 40px;
+                        text-align: center;
+                        color: #cbcbcb;
+                        cursor: pointer;
+                        user-select: none;
+                        box-sizing: border-box;
+                        transition: 1s;
+                        background-color: rgba(110, 110, 110, 0.8);
+                        top: 50px;
+                    }
+                    
+                    #search {
+                        left: 950px;
+                        width: 60px;
+                    }
+                    
+                    #textbox {
+                        left: 800px;
+                        width: 150px;
+                    }
+                    
+                    #globalArea {
+                        height: 100%;
+                        width: 100%;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    
+                    #infoFilter {
+                        position: absolute;
+                        left: 26%;
+                        top: 20%;
+                        width: 50%;
+                        height: 50%;
+                        line-height: 50px;
+                        text-align: center;
+                        color: white;
+                        font-size: 20px;
+                        background-color: rgba(106, 107, 111, 0.6);
+                        display: none;
+                    }
+                    /* Pasirinktos šalies teksto laukas */
+                    
+                    #countryChosen {
+                        width: 20%;
+                        height: 2%;
+                    }
+                    
+                    #countryFilter {
+                        width: 100%;
+                        height: 10%;
+                    }
+                    
+                    .alert-close {
+                        background: rgba(255, 255, 255, 0.1);
+                        -webkit-border-radius: 50%;
+                        -moz-border-radius: 50%;
+                        -ms-border-radius: 50%;
+                        -o-border-radius: 50%;
+                        border-radius: 50%;
+                        -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4), inset 0 -1px 2px rgba(255, 255, 255, 0.25);
+                        -moz-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4), inset 0 -1px 2px rgba(255, 255, 255, 0.25);
+                        box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4), inset 0 -1px 2px rgba(255, 255, 255, 0.25);
+                        color: #FFFFFF;
+                        cursor: pointer;
+                        font-size: 18px;
+                        font-weight: normal;
+                        height: 22px;
+                        line-height: 24px;
+                        position: absolute;
+                        right: 11px;
+                        text-align: center;
+                        top: 9px;
+                        -webkit-transition: color 0.2s ease-in-out;
+                        -moz-transition: color 0.2s ease-in-out;
+                        -o-transition: color 0.2s ease-in-out;
+                        transition: color 0.2s ease-in-out;
+                        width: 22px;
+                    }
+                </style>
+    </head>
 
-position: absolute;
-height: 40px;
-line-height: 40px;
-text-align: center;
-color: #cbcbcb;
-cursor: pointer;
-user-select: none;
-box-sizing: border-box;
-transition: 1s;
-background-color: rgba(110, 110, 110, 0.8);
-top: 50px;
-}
+    <body>
+        <input id="textbox" type="text" name="fname">
+        <div id="search">Search</div>
+        <script src="js/search.js"></script>
 
-#search {
-  left: 950px;
-  width: 60px;
-}
-#textbox{
-  left: 800px;
-  width: 150px;
-}
+        <div id="globalArea"></div>
+        <div id="infoFilter">
+            <div class="alert-close">×</div>
+            <div id="countryChosen"></div>
+            <div id="countryFilter"></div>
+            <button id="button_1">Try it</button>
+            <input type="text" placeholder="Type something..." id="myInput">
+            <font size="4" face="Calibri">
+    <table class = "table table-responsive" style = "max-height: 77%">
+    <thead id="infoTHead">
+    <tr><th>Destination name</th><th>Duration</th><th>Rating</th></tr>
+    </thead>
+    <tbody id="testas">
+    </tbody>
+    </table>
+    <!-- Lentelės išjungimo scriptas -->
+  <script>
+              $(document).ready(function(c) {
+                $('.alert-close').on('click', function(c){
+                  document.getElementById("textbox").style.display = '';
+                  document.getElementById("search").style.display = '';
+                  $(this).parent().fadeOut('slow', function(c){
+              });
+        });   
+  });
+  </script>
+  <script src="js/countryselect.js"></script>
 
-        #globalArea {
-
-            height: 100%;
-            width: 100%;
-            margin: 0;
-            padding: 0;
-        }
-
-        #infoFilter {
-
-            position: absolute;
-            left: 26%;
-            top: 20%; 
-            width: 50%;
-            height: 50%;
-            line-height: 50px;
-            text-align: center; 
-            color: white;
-            font-size: 20px;
-            background-color: rgba(106,   107, 111, 0.6);
-            display: none;
-        }
-/* Pasirinktos šalies teksto laukas */
-      #countryChosen {
-            width: 20%;
-            height: 2%;
-        }
-        #countryFilter {
-            width: 100%;
-            height: 10%;
-        }
-        
-        .alert-close {
-           background: rgba(255,255,255,0.1);
-            -webkit-border-radius: 50%;
-            -moz-border-radius: 50%;
-          -ms-border-radius: 50%;
-            -o-border-radius: 50%;
-          border-radius: 50%;
-            -webkit-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4),inset 0 -1px 2px rgba(255,255,255,0.25);
-            -moz-box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4),inset 0 -1px 2px rgba(255,255,255,0.25);
-            box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.4),inset 0 -1px 2px rgba(255,255,255,0.25);
-            color: #FFFFFF;
-            cursor: pointer;
-            font-size: 18px;
-            font-weight: normal;
-            height: 22px;
-            line-height: 24px;
-            position: absolute;
-            right: 11px;
-            text-align: center;
-            top: 9px;
-            -webkit-transition: color 0.2s ease-in-out;
-            -moz-transition: color 0.2s ease-in-out;
-            -o-transition: color 0.2s ease-in-out;
-          transition: color 0.2s ease-in-out;
-            width: 22px;
-}
-</style>
-</head>
-<body>
-<input id="textbox" type="text" name="fname">
-<div id="search">Search</div> 
-<script src="js/search.js"></script>
-
-
-    <div id="globalArea"></div>
-<div id="infoFilter">
-    <div class="alert-close">×</div>
-    <div id="countryChosen"></div>
-    <div id="countryFilter"></div>
-    <button id="button_1">Try it</button>
-    <input type="text" placeholder="Type something..." id="myInput">
-    <font size="4" face="Calibri">
-  <table class = "table table-responsive" style = "max-height: 77%">
-  <thead>
-   <tr><th>Destination name</th><th>Duration</th><th id="testukaz">Rating</th></tr>
-  </thead>
-   <tbody id="testas">
-   </tbody>
-  </table>
-  <!-- Lentelės išjungimo scriptas -->
-<script>
-            $(document).ready(function(c) {
-              $('.alert-close').on('click', function(c){
-                document.getElementById("textbox").style.display = '';
-                document.getElementById("search").style.display = '';
-                $(this).parent().fadeOut('slow', function(c){
-            });
-      });   
-});
-</script>
-<script src="js/countryselect.js"></script>
-
-</body>
-</html>
+  </body>
+  
+  </html>
--- TraveLog/js/countryselect.js
+++ TraveLog/js/countryselect.js
@@ -9,7 +9,9 @@ $("#button_1").click(function(e) {
 e.preventDefault();
 document.getElementById("myInput").defaultValue = 0;
 var inputVal = document.getElementById("myInput").value;
-$("#testas").load(window.location + " #testas");
+$("#testas").text("")//.load(window.location + " #testas");
+
+//
 $.ajax({
     url: "Duombaze.php",
     type: "POST",
@@ -18,12 +20,14 @@ $.ajax({
         'inputKaina': inputVal,
     },
     success: function(inputData) {
+
             document.getElementById("button_1").onclick = null;
         let testas = document.getElementById("testas");
            if (inputData.search("nulis") != -1) {
                testas.insertAdjacentText("beforeend", "no matches found"); // jeigu duombazėje nėra duomenų apie šalį;
            }
            else {
+
                let test = JSON.parse(inputData);
                for (var i = 0; i < test.length; i++) {
                    testas.insertAdjacentHTML("beforeend", "<tr> <td align='left'> <button onClick='myScript("+ test[i].ID +")'>" + test[i].name + " </button></td><td>"
@@ -36,7 +40,13 @@ $.ajax({
 
 function myScript(ID)
 {
-    
+    $('#testas').fadeOut(1);
+    $('#button_1').fadeOut(1);
+    $('#myInput').fadeOut(1);
+    $("#countryChosen").fadeOut(1);
+    $("#infoFilter").fadeIn(1000);
+    $('#testas').fadeIn(1);
+    document.getElementById("infoTHead").innerHTML ="<tr><th>Check In</th><th>Price</th><th>Link</th></tr>";
     $.ajax({
         url: "Duombaze2.php",
         type: "POST",
@@ -44,7 +54,18 @@ function myScript(ID)
             'inputID': ID,
         },
         success: function(inputData) {
+                document.getElementById("button_1").onclick = null;
+                document.getElementById("testas").innerText = "";
+
+                let testas = document.getElementById("testas");
                 let test = JSON.parse(inputData);
+                $("#countryFilter").text(test[0 ].name);
+                for (var i = 0; i < test.length; i++) {
+                    testas.insertAdjacentHTML("beforeend", "<tr> <td align='left'> " 
+                    + test[i].checkIn + "</td> <td align='left'>" 
+                    + test[i].price+ 
+                    "</td> <td align='left'>"+ "<a href="+test[i].link +"class="+"button"+" target="+"_blank"+" >Click Here</a>"+"</td></tr>");
+                 }
             }
         })
         
@@ -55,16 +76,18 @@ function callfirst(selectedCountry)
     $("#countryChosen").text("Chosen country: " +selectedCountry.name);
     $("#infoFilter").fadeIn(1000)
     $("#countryFilter").text("Select filters:");
-
+    document.getElementById("infoTHead").innerHTML ="<tr><th>Destination name</th><th>Duration</th><th>Rating</th></tr>";
     setTimeout(function() {
         $("#infoFilter");
 
     }, 1000);
     localStorage.setItem('CountryName', selectedCountry.name); // sukuriamas lokalus kintamasis šalies pavadinimui
-    let testas = document.getElementById("testas"); // gauna elementą pagal jo ID
-    testas.innerHTML = null; // keičia duomenis kitos valstybės
+    document.getElementById("testas").innerText = ""; // gauna elementą pagal jo ID
     document.getElementById("textbox").style.display = 'none'; // paslepia paieškos laukus
     document.getElementById("search").style.display = 'none';  // paslepia paieškos laukus
+    $('#button_1').fadeIn(1000);
+    $('#myInput').fadeIn(1000);
+    $("#countryChosen").fadeIn(1);
 }
 
 $.ajax({
