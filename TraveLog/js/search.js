e067f3c4f8507254c899177492db1bab70e345ee
viliusgec <55325682+viliusgec@users.noreply.github.com>
2020-04-23 12:13:15 +0300

    Update search.js

--- TraveLog/js/search.js
+++ TraveLog/js/search.js
@@ -1,7 +1,27 @@
 $( "#search" ).click( function () {
 
-    // use the switchCountry() API to directly change the clicked country without clicked on the surface
     var countrydata = document.getElementById("textbox").value;
-    controller.switchCountry( countrydata );
-    //controller.switchCountry( "LT" );
+
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("data/countryISO.json", function(text){
+    var obj = JSON.parse(text, function (name, value) {
+        if (name.toLowerCase() == countrydata.toLowerCase()) {
+            console.log(value);
+            controller.switchCountry(value);
+          return value;
+        }
+      });
+});
 } );
