8a4030d274d66ee5526b47cc371a2026b7d864ae
necrorez <dalius.visokinskas@gmail.com>
2020-06-03 21:03:14 +0300

    Updated to work with images

--- TraveLog/python/Listing_Updater.py
+++ TraveLog/python/Listing_Updater.py
@@ -19,17 +19,13 @@ def main(word1=first_arg):
         database="travelog"
       )
       mycursor = mydb.cursor() 
-      mycursor.execute("TRUNCATE TABLE country_has_listing")
-      mycursor.execute("TRUNCATE TABLE individual")
-      mycursor.execute("DELETE FROM listing")
-      mycursor.execute("ALTER TABLE listing AUTO_INCREMENT = 1")
-      mydb.commit()
+      
       
       with open(word1) as json_file:
             data=json.load(json_file)
             for d in data['Data']:
-                  toPrint1 = (checkIfNull(d['Destination Name']) , checkIfNull(d['Duration']) , checkIfNull(d['Rating']) , checkIfNull(d['Link']))
-                  mycursor.execute("INSERT INTO listing(destinationName, duration, rating, link) VALUES (%s, %s, %s, %s)", toPrint1)
+                  toPrint1 = (checkIfNull(d['Destination Name']) , checkIfNull(d['Duration']) , checkIfNull(d['Rating']) , checkIfNull(d['Link']), d['Image'])
+                  mycursor.execute("INSERT INTO listing(destinationName, duration, rating, link,imageLink) VALUES (%s, %s, %s, %s,%s)", toPrint1)
                   lastID=mycursor.lastrowid
                   for c in d['Countries']:
                         mycursor.execute("INSERT INTO country_has_listing(listingId,countryId) VALUES (%s,%s)", (lastID,c))
