4c4ec859cec1be9b9db57fe8b1056f366262e1e2
necrorez <dalius.visokinskas@gmail.com>
2020-06-03 21:01:39 +0300

    Fixed bugg
    Before didn't create or truncate json file

--- TraveLog/python/selector.sh
+++ TraveLog/python/selector.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-directory=`python3 NameSelector.py`
+directory=`python3 NameSelector.py "Pazintines"`
 direct=`echo "$directory"`
+FileCreate=`python3 FileCreator.py "$direct"`
 script=`./crawlerLauncher.sh "Novaturas_Crawler_Pazintines.py" "$direct" "https://www.novaturas.lt/pazintines-keliones"`
 
 directory2=`python3 NameSelector.py "Keliones"`
 direct2=`echo "$directory2"`
-script2=`./test.sh "$direct2"`
-isOver=`echo "$script2" | grep 'Baigta' -c`
-if [ $isOver -ge 1 ]
-then
+FileCreate2=`python3 FileCreator.py "$direct2"`
+script2=`./kelionesLauncher.sh "$direct2"`
+wait
+
 python3 Listing_Truncate.py
 python3 Listing_Updater.py "$direct2"
-fi
+python3 Listing_Updater.py "$direct"
